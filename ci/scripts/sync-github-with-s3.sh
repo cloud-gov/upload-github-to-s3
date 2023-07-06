5d0fce3341ecbbaabce512172b842c73d68daa00
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-06 10:29:35 -0400

    testing of script for sync with s3

---
+++ ci/scripts/sync-github-with-s3.sh
@@ -0,0 +1,18 @@
+MYJSONSTRING=$(gh repo list cloud-gov --json name,updatedAt --limit 500 --no-archived)
+TWO_DAYS_AGO=$(date -j -v -2d +"%Y-%m-%dT%H:%M:%S%:z")
+for row in $(echo "${MYJSONSTRING}" | jq -r '.[] | @base64'); do
+    _jq() {
+      echo ${row} | base64 --decode | jq -r ${1}
+    }
+    UPLOAD_DATE=$(_jq '.updatedAt')
+    if [[ "${TWO_DAYS_AGO}" < "${UPLOAD_DATE}" ]];
+    then 
+      TEST=$(_jq '.name')
+      git clone https://github.com/cloud-gov/$TEST
+      zip -r ${TEST}.zip $TEST
+      sudo rm -r $TEST
+      aws s3 cp --sse AES256 ${TEST}.zip  s3://github-backups/${TEST}.zip
+    fi
+   
+
+done
