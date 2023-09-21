1961d93efb623e15fbff05495bc9516e1afb437f
Jason A. Gambino <jason.gambino@gsa.gov>
2023-09-21 13:17:53 -0400

    echo test

--- ci/scripts/sync-github-with-s3.sh
+++ ci/scripts/sync-github-with-s3.sh
@@ -1,19 +1,20 @@
-MYJSONSTRING=$(gh repo list cloud-gov --json name,updatedAt --limit 500 --no-archived)
-TWO_DAYS_AGO=$(date -j -v -2d +"%Y-%m-%dT%H:%M:%S%:z")
-for row in $(echo "${MYJSONSTRING}" | jq -r '.[] | @base64'); do
-    _jq() {
-      echo ${row} | base64 --decode | jq -r ${1}
-    }
-    UPLOAD_DATE=$(_jq '.updatedAt')
-    if [[ "${TWO_DAYS_AGO}" < "${UPLOAD_DATE}" ]];
-    then
-      TEST=$(_jq '.name')
-      git clone https://github.com/cloud-gov/$TEST
-      zip -r ${TEST}.zip $TEST
-      sudo rm -r $TEST
-      aws s3 cp --sse AES256 ${TEST}.zip  s3://github-backups/${TEST}.zip
-      #rm ${TEST}.zip
-    fi
+# MYJSONSTRING=$(gh repo list cloud-gov --json name,updatedAt --limit 500 --no-archived)
+# TWO_DAYS_AGO=$(date -j -v -2d +"%Y-%m-%dT%H:%M:%S%:z")
+# for row in $(echo "${MYJSONSTRING}" | jq -r '.[] | @base64'); do
+#     _jq() {
+#       echo ${row} | base64 --decode | jq -r ${1}
+#     }
+#     UPLOAD_DATE=$(_jq '.updatedAt')
+#     if [[ "${TWO_DAYS_AGO}" < "${UPLOAD_DATE}" ]];
+#     then
+#       TEST=$(_jq '.name')
+#       git clone https://github.com/cloud-gov/$TEST
+#       zip -r ${TEST}.zip $TEST
+#       sudo rm -r $TEST
+#       aws s3 cp --sse AES256 ${TEST}.zip  s3://github-backups/${TEST}.zip
+#       #rm ${TEST}.zip
+#     fi
 
 
-done
+# done
+echo "van and jason pair"
