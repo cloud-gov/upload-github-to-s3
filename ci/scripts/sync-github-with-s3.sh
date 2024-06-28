fd22a2b32a3a6c3d1d9a82244677642e01eaa24c
Jason A. Gambino <jason.gambino@gsa.gov>
2024-06-28 12:43:57 -0400

    trying to compare days

--- ci/scripts/sync-github-with-s3.sh
+++ ci/scripts/sync-github-with-s3.sh
@@ -3,13 +3,12 @@ tar -xzf gh-release/gh_2.52.0_linux_amd64.tar.gz
 GH=gh_2.52.0_linux_amd64/bin/gh
 export GH_TOKEN=${ACCESS_TOKEN}
 MYJSONSTRING=$($GH repo list cloud-gov --json name,updatedAt --limit 500 --no-archived)
-TWO_DAYS_AGO=$(date -d "2 days ago"  +%s)
+TWO_DAYS_AGO=$(date -d "2 days ago"  +"%Y-%m-%dT%H:%M:%S%:z")
 echo $TWO_DAYS_AGO
 for row in $(echo "${MYJSONSTRING}" | jq -r '.[] | @base64'); do
     echo "peanut butter"
     _jq() {
       echo ${row} | base64 --decode | jq -r ${1}
-      echo "jelly"
     }
     UPLOAD_DATE=$(_jq '.updatedAt')
     if [ "`date --date \"$TWO_DAYS_AGO\" +%s`"  -lt "`date --date \"$UPLOAD_DATE\" +%s`"]
