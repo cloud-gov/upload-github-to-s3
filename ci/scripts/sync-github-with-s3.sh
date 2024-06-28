78ab03fb7e44e2090d99c1cc0422ec1c27ff44ff
Jason A. Gambino <jason.gambino@gsa.gov>
2024-06-28 12:07:47 -0400

    figuring out date, forgot the actual command

--- ci/scripts/sync-github-with-s3.sh
+++ ci/scripts/sync-github-with-s3.sh
@@ -3,8 +3,7 @@ tar -xzf gh-release/gh_2.52.0_linux_amd64.tar.gz
 GH=gh_2.52.0_linux_amd64/bin/gh
 export GH_TOKEN=${ACCESS_TOKEN}
 MYJSONSTRING=$($GH repo list cloud-gov --json name,updatedAt --limit 500 --no-archived)
-date --help
-date --version
+echo $MYJSONSTRING
 TWO_DAYS_AGO=$(date -d "2 days ago"  +"%Y-%m-%dT%H:%M:%S%:z")
 for row in $(echo "${MYJSONSTRING}" | jq -r '.[] | @base64'); do
     _jq() {
