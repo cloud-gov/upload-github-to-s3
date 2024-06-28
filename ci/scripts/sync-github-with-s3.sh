3758ff41a1cbcfa22479b1b1fff568dd4f3d27d6
Jason A. Gambino <jason.gambino@gsa.gov>
2024-06-28 15:30:39 -0400

    getting ready to do aws step

--- ci/scripts/sync-github-with-s3.sh
+++ ci/scripts/sync-github-with-s3.sh
@@ -2,18 +2,15 @@
 tar -xzf gh-release/gh_2.52.0_linux_amd64.tar.gz
 GH=gh_2.52.0_linux_amd64/bin/gh
 export GH_TOKEN=${ACCESS_TOKEN}
-MYJSONSTRING=$($GH repo list cloud-gov --json name,updatedAt --limit 500 --no-archived)
+LIST_OF_REPOS=$($GH repo list cloud-gov --json name,updatedAt --limit 500 --no-archived)
 TWO_DAYS_AGO=$(date -d "2 days ago"  +"%Y-%m-%dT%H:%M:%S%:z")
-echo $TWO_DAYS_AGO
-for row in $(echo "${MYJSONSTRING}" | jq -r '.[] | @base64'); do
-    echo "peanut butter"
+for row in $(echo "${LIST_OF_REPOS}" | jq -r '.[] | @base64'); do
     _jq() {
       echo ${row} | base64 --decode | jq -r ${1}
     }
-    UPLOAD_DATE=$(_jq '.updatedAt')
-    if [ "`date --date \"$TWO_DAYS_AGO\" +%s`"  -lt "`date --date \"$UPLOAD_DATE\" +%s`" ];
+    LAST_UPDATE=$(_jq '.updatedAt')
+    if [ "`date --date \"$TWO_DAYS_AGO\" +%s`"  -lt "`date --date \"$LAST_UPDATE\" +%s`" ];
     then
-    echo "sandwhich"
     #   TEST=$(_jq '.name')
     #   git clone https://github.com/cloud-gov/$TEST
     #   zip -r ${TEST}.zip $TEST
