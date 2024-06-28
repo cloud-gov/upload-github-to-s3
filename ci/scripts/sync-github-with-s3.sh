5632b45884eb10963efa5343f1a1d6a6581486bc
Jason A. Gambino <jason.gambino@gsa.gov>
2024-06-28 12:35:40 -0400

    bash is not shell

--- ci/scripts/sync-github-with-s3.sh
+++ ci/scripts/sync-github-with-s3.sh
@@ -3,7 +3,7 @@ tar -xzf gh-release/gh_2.52.0_linux_amd64.tar.gz
 GH=gh_2.52.0_linux_amd64/bin/gh
 export GH_TOKEN=${ACCESS_TOKEN}
 MYJSONSTRING=$($GH repo list cloud-gov --json name,updatedAt --limit 500 --no-archived)
-TWO_DAYS_AGO=$(date -d "2 days ago"  +"%Y-%m-%dT%H:%M:%S%:z")
+TWO_DAYS_AGO=$(date -d "2 days ago"  +%s)
 echo $TWO_DAYS_AGO
 for row in $(echo "${MYJSONSTRING}" | jq -r '.[] | @base64'); do
     echo "peanut butter"
