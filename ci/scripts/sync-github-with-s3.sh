622d0d68dd09d64eae91072155b55dc4eeef2ca0
Jason A. Gambino <jason.gambino@gsa.gov>
2024-06-28 12:11:39 -0400

    figuring out for loop

--- ci/scripts/sync-github-with-s3.sh
+++ ci/scripts/sync-github-with-s3.sh
@@ -8,16 +8,18 @@ for row in $(echo "${MYJSONSTRING}" | jq -r '.[] | @base64'); do
     echo "peanut butter"
     _jq() {
       echo ${row} | base64 --decode | jq -r ${1}
+      echo "jelly"
     }
     UPLOAD_DATE=$(_jq '.updatedAt')
-    # if [[ "${TWO_DAYS_AGO}" < "${UPLOAD_DATE}" ]];
-    # then
+    if [[ "${TWO_DAYS_AGO}" < "${UPLOAD_DATE}" ]];
+    then
+    echo "sandwhich"
     #   TEST=$(_jq '.name')
     #   git clone https://github.com/cloud-gov/$TEST
     #   zip -r ${TEST}.zip $TEST
     #   sudo rm -r $TEST
     #   aws s3 cp --sse AES256 ${TEST}.zip  s3://github-backups/${TEST}.zip
-    # fi
+    fi
 
 
 done
