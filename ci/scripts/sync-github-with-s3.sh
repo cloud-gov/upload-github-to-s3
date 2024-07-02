b2474ab55e1a4e032de7f2b2116c5384b17ed003
Jason A. Gambino <jason.gambino@gsa.gov>
2024-07-02 16:18:04 -0400

    quiet

--- ci/scripts/sync-github-with-s3.sh
+++ ci/scripts/sync-github-with-s3.sh
@@ -11,11 +11,12 @@ for row in $(echo "${LIST_OF_REPOS}" | jq -r '.[] | @base64'); do
     LAST_UPDATE=$(_jq '.updatedAt')
     if [ "`date --date \"$TWO_DAYS_AGO\" +%s`"  -lt "`date --date \"$LAST_UPDATE\" +%s`" ];
     then
+      echo Success
       REPOSITORY=$(_jq '.name')
-      git clone https://github.com/cloud-gov/$REPOSITORY
+      git clone https://github.com/cloud-gov/$REPOSITORY --quiet
       tar czf ${REPOSITORY}.tar.gz $REPOSITORY
       sudo rm -r $REPOSITORY
-      aws s3 cp --sse AES256 ${REPOSITORY}.tar.gz  s3://github-backups/${REPOSITORY}.tar.gz
+      aws s3 cp --sse AES256 ${REPOSITORY}.tar.gz  s3://github-backups/${REPOSITORY}.tar.gz --quiet
       sudo rm -r ${REPOSITORY}.tar.gz
     fi
 
