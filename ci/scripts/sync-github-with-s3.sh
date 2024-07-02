85dafd63e5d82868acadf0fb37e441f35194457a
Jason A. Gambino <jason.gambino@gsa.gov>
2024-07-02 10:37:29 -0400

    adding in tar.gz instead

--- ci/scripts/sync-github-with-s3.sh
+++ ci/scripts/sync-github-with-s3.sh
@@ -14,10 +14,10 @@ for row in $(echo "${LIST_OF_REPOS}" | jq -r '.[] | @base64'); do
     then
       REPOSITORY=$(_jq '.name')
       git clone https://github.com/cloud-gov/$REPOSITORY
-      zip -r ${REPOSITORY}.zip $REPOSITORY
+      tar czf ${REPOSITORY}.tar.gz $REPOSITORY
       sudo rm -r $REPOSITORY
-      aws s3 cp --sse AES256 ${REPOSITORY}.zip  s3://github-backups/${REPOSITORY}.zip
-      sudo rm -r ${REPOSITORY}.zip
+      aws s3 cp --sse AES256 ${REPOSITORY}.tar.gz  s3://github-backups/${REPOSITORY}.tar.gz
+      sudo rm -r ${REPOSITORY}.tar.gz
     fi
 
 
