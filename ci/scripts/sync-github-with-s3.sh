f26cbfa8f0f00a26e20d4c8a2959dbccb6da7c51
Christopher Weibel <christopher.weibel@gsa.gov>
2025-11-05 11:08:40 -0500

    Adding echo to see what is going on via concourse output

--- ci/scripts/sync-github-with-s3.sh
+++ ci/scripts/sync-github-with-s3.sh
@@ -16,7 +16,10 @@ for row in $(echo "${LIST_OF_REPOS}" | jq -r '.[] | @base64'); do
       git clone https://github.com/cloud-gov/$REPOSITORY --quiet
       tar czf ${REPOSITORY}.tar.gz $REPOSITORY
       rm -r $REPOSITORY
+      echo "Uploading ${REPOSITORY} to s3://github-backups/${REPOSITORY}.tar.gz
       aws s3 cp --sse AES256 ${REPOSITORY}.tar.gz  s3://github-backups/${REPOSITORY}.tar.gz --quiet
       rm -r ${REPOSITORY}.tar.gz
     fi
 done
+
+echo "~fin~"
